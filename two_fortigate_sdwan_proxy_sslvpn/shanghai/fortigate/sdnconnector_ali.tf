80195214f6cca36b40a8c217909acd437c427152
yagosys <yagosys@gmail.com>
2022-03-09 15:03:36 +0800

    update

--- two_fortigate_sdwan_proxy_sslvpn/shanghai/fortigate/sdnconnector_ali.tf
+++ two_fortigate_sdwan_proxy_sslvpn/shanghai/fortigate/sdnconnector_ali.tf
@@ -5,6 +5,6 @@ resource "fortios_system_sdnconnector" "sdn_connector_aliyun_sdn" {
     type               = "alicloud"
     update_interval    = 60
     use_metadata_iam   = "disable"
-    access_key = "LTAI5t62YzQckDoC8hDMMP6x"
-    secret_key = "TWH7YffdaDkpYgO4sLwKvUdQzdASae"
+    access_key = ""
+    secret_key = ""
 }
