3c1d9c898d10bdc00b755e08951f1ff1c57f00b0
yagosys <yagosys@gmail.com>
2022-03-09 15:15:01 +0800

    update

--- two_fortigate_sdwan_proxy_sslvpn/shanghai/test.sh
+++
@@ -1,5 +0,0 @@
-for region in us-east-1 us-west-1  eu-west-1 me-east-1 eu-central-1
-do
-   terraform apply --auto-approve --var region=$region
-   terraform destroy --auto-approve --var region=$region
-done
