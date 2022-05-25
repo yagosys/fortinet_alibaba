472203f989bc573fede1c41bd3dc36e82124c5d9
yagosys <yagosys@gmail.com>
2022-05-25 09:31:41 +0800

    update output with mgmt eip

--- fortigate_deployment/fortigate/output.tf
+++ fortigate_deployment/fortigate/output.tf
@@ -5,3 +5,7 @@ output "PrimaryFortigatePublicIP" {
 output "PrimaryFortigateID" {
   value =  var.fortigate["internet_max_bandwidth_out"] !="0" ? alicloud_instance.PrimaryFortigate.*.id : null 
 }
+
+output "MgmtFortigateIP" {
+ value = var.mgmt_eip==1 ? alicloud_eip.MgmtEip.*.ip_address: null
+}
